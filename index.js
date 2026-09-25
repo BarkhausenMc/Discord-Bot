81a71e5998c2c9697481e9352a32d4f67913875e
Flo <you@example.com>
2026-09-25 23:27:35 +0200

    fix v1 komplett

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayintentBits, ChannelType} = require('discord.js');
+const { Client, GatewayIntentBits, ChannelType } = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -8,32 +8,32 @@ const client = new Client({
 });
 
 async function updateMemberCount() {
-    const guild = client.guilds.cache.first();
-
-    if (!guild) return;
-
-    await guild.members.fetch();
-
-    const totalMembers = guild.MemberCount;
-    const onlineMembers = guild.presences.cache.size;
-
-    const channel = await guild.channels.fetch(process.env.MEMBER_COUNT_CHANNEL_ID);
-
-    if (channel && channel.type === ChannelType.Voice) {
-        const name = `👥・Members: ${totalMembers} | 🟢・Online: ${onlineMembers}`; 
-
-        if (channel.name !== name) {
-            await channel.setName(name);
-        }
+  const guild = client.guilds.cache.first();
+  
+  if (!guild) return;
+  
+  await guild.members.fetch();
+  
+  const totalMembers = guild.memberCount;
+  const onlineMembers = guild.presences.cache.size;
+  
+  const channel = await guild.channels.fetch(process.env.MEMBER_COUNT_CHANNEL_ID);
+  
+  if (channel && channel.type === ChannelType.Voice) {
+    const name = `👥・Members: ${totalMembers} | 🟢・Online: ${onlineMembers}`;
+    
+    if (channel.name !== name) {
+      await channel.setName(name);
     }
+  }
 }
 
-client.on('ready', async () =>{
-    console.log(`Logged in as ${client.user.tag}`);
-
-    await updateMemberCount();
-
-    setInterval(updateMemberCount, 60000);
+client.on('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  
+  await updateMemberCount();
+  
+  setInterval(updateMemberCount, 60000);
 });
 
 client.on('guildMemberAdd', updateMemberCount);
@@ -43,3 +43,6 @@ client.on('presenceUpdate', () => {
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
+
+
+
